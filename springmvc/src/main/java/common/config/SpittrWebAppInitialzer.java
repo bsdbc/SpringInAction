1917698edbf3a718a54a2442e792dcd955936c71
wangwenrui <wangwenrui@zdjizhi.com>
2019-12-30 18:32:02 +0800

    feat:新增springmvc modules

---
+++ springmvc/src/main/java/common/config/SpittrWebAppInitialzer.java
@@ -0,0 +1,17 @@
+package common.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitialzer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
